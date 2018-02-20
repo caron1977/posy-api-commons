d769e0e1c6b107cddcb89abf2e5af37da7cadc35
grimmer <ron.grimmer@fhws.de>
2018-02-20 16:16:54 +0100

    v.1.2.5: added xml annotations to WorkflowStatus

--- src/main/java/de/fhws/posy/commons/enums/WorkflowStatus.java
+++ src/main/java/de/fhws/posy/commons/enums/WorkflowStatus.java
@@ -1,14 +1,23 @@
 package de.fhws.posy.commons.enums;
 
+import javax.xml.bind.annotation.XmlEnumValue;
+
 public enum WorkflowStatus {
 
 	//@formatter:off
+	@XmlEnumValue(value="Entwurf")
 	TEMPLATE("Entwurf"),
+	@XmlEnumValue(value="Mitzeichnen")
 	SIGNGRADES("Mitzeichnen"),
+	@XmlEnumValue(value="Freigegeben")
 	RELEASED("Freigegeben"), 
+	@XmlEnumValue(value="Genehmigt")
 	APPROVED("Genehmigt"), 
+	@XmlEnumValue(value="Abgelehnt")
 	REJECTED("Abgelehnt"),
+	@XmlEnumValue(value="Zurückgezogen")
 	WITHDRAWN("Zurückgezogen"),
+	@XmlEnumValue(value="Abgesagt")
 	CANCELED("Abgesagt");
 	//@formatter:on
 
@@ -23,4 +32,5 @@ public enum WorkflowStatus {
 
 		return display;
 	}
+
 }
